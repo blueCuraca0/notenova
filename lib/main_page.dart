fd23e4cdc29d25be282ff83543daf076db6ce7fb
anastasiiavelma <anastasiia.velma@nure.ua>
2024-05-08 22:54:10 +0300

    fixed issues with cubit

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:notenova/features/cards/presentation/pages/card_stacks_page.dart';
-import 'package:notenova/features/summary/presentation/summary_page.dart';
+import 'package:notenova/features/summary/presentation/pages/summary_page.dart';
 
 import 'core/utils/c_routes.dart';
 import 'core/widgets/bottom_nav_bar.dart';
@@ -55,17 +55,14 @@ class MainPage extends StatelessWidget {
       child: Stack(
         fit: StackFit.expand,
         children: [
-          //page itself
-          // Navigator(
-          //   key: _navigatorKey,
-          //   key: _navigatorKey,
-          //   initialRoute: CRoutes.routeHomepage,
-          //   onGenerateRoute: _onGenerateRoute,
-          // ),
-          // ),
+          Navigator(
+            key: _navigatorKey,
+            initialRoute: CRoutes.routeHomepage,
+            onGenerateRoute: _onGenerateRoute,
+          ),
 
-          const SummaryPage(),
-          //const CardStacksPage(),
+          // const SummaryPage(),
+          // //const CardStacksPage(),
           Column(
             children: [
               const Expanded(child: SizedBox()),
